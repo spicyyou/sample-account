c4f463f6a58e9e143a97e74a3cd310a624fc97ae
spicyyou <hyj5419@naver.com>
2021-02-08 16:30:03 +0900

    에러 해결

--- src/main/java/com/nhn/rookie8/sample/account/helloController.java
+++ src/main/java/com/nhn/rookie8/sample/account/helloController.java
@@ -2,10 +2,12 @@ package com.nhn.rookie8.sample.account;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class helloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello world";
     }
